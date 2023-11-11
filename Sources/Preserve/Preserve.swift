6c3ffda6c75fe3112e12d47990348474433dc3bc
BlueSkyAnimal <101543434+BlueSkyAnimal@users.noreply.github.com>
2023-11-11 09:38:18 +0900

    Preserve.itemにDataをそのまま返す関数の追加

--- Sources/Preserve/Preserve.swift
+++ Sources/Preserve/Preserve.swift
@@ -9,36 +9,42 @@ import SwiftUI
 
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, xrOS 1.0, *)
 public class Preserve {
-  public enum Store {
-    case userDefaults(UserDefaults = .standard)
-    case fileManager(FileManager.SearchPathDirectory = .documentDirectory)
-  }
-  
-  public static func save(_ value: Encodable, key name: String, to store: Store = .fileManager(.documentDirectory)) throws {
-    let data = try value.encoded()
-    
-    switch store {
-    case .userDefaults(let store):
-      store.set(data, forKey: name)
-    case .fileManager(let directory):
-      try FileManager.save(name, data: data, directory: directory)
+    public enum Store {
+        case userDefaults(UserDefaults = .standard)
+        case fileManager(FileManager.SearchPathDirectory = .documentDirectory)
     }
-  }
-  
-  public static func item<T: Codable>(_ type: T.Type = T.self, key name: String, from store: Store = .fileManager(.documentDirectory)) throws -> T {
-    func data() throws -> Data {
-      switch store {
-      case .userDefaults(let store):
-        guard let content = store.data(forKey: name) else {
-          throw FileManagerError.fileNotFound
+    
+    public static func save(_ value: Encodable, key name: String, to store: Store = .fileManager(.documentDirectory)) throws {
+        let data = try value.encoded()
+        
+        switch store {
+        case .userDefaults(let store):
+            store.set(data, forKey: name)
+        case .fileManager(let directory):
+            try FileManager.save(name, data: data, directory: directory)
         }
-        return content
-      case .fileManager(let directory):
-        return try FileManager.item(name, directory: directory)
-      }
     }
     
-    return try data().decoded(T.self)
-  }
+    public static func item<T: Codable>(_ type: T.Type = T.self, 
+                                        key name: String,
+                                        from store: Store = .fileManager(.documentDirectory)
+    ) throws -> T {
+        let data: Data = try item(key: name, from: store)
+        return try data.decoded(T.self)
+    }
+    
+    public static func item(key name: String,
+                            from store: Store = .fileManager(.documentDirectory)
+    ) throws -> Data {
+        switch store {
+        case .userDefaults(let store):
+            guard let content = store.data(forKey: name) else {
+                throw FileManagerError.fileNotFound
+            }
+            return content
+        case .fileManager(let directory):
+            return try FileManager.item(name, directory: directory)
+        }
+    }
 }
 
